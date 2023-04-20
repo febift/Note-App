19758c92b6042883804c0136816f60133ce67cf2
ekahanny <ekahnny@gmail.com>
2023-04-20 23:01:32 +0800

    Menyelesaikan project

--- NoteApp/app/src/main/java/com/example/noteapp/MainActivity.java
+++ NoteApp/app/src/main/java/com/example/noteapp/MainActivity.java
@@ -1,32 +1,100 @@
 package com.example.noteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.Query;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    ImageButton menuBtn;
+    RecyclerView rv_note_item;
+    ImageButton menu_btn;
+    FloatingActionButton add_note_btn;
+    NoteAdapter noteAdapter;
+    private ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.recycler_view);
-        menuBtn = findViewById(R.id.menu_btn);
+        rv_note_item = findViewById(R.id.rv_note_item);
+        menu_btn = findViewById(R.id.menu_btn);
+        add_note_btn = findViewById(R.id.add_note_btn);
 
-        menuBtn.setOnClickListener((v)->showMenu());
+        add_note_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), NoteDetailsActivity.class));
+            }
+        });
+
+        menu_btn.setOnClickListener((v)->showMenu());
+        setupRecyclerView();
     }
 
     void showMenu() {
+        PopupMenu popupMenu = new PopupMenu(MainActivity.this, menu_btn);
+        popupMenu.getMenu().add("Logout");
+        popupMenu.show();
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                if (menuItem.getTitle() == "Logout") {
+                    FirebaseAuth.getInstance().signOut();
+                    progressDialog = new ProgressDialog(MainActivity.this);
+                    progressDialog.setTitle("Loading");
+                    progressDialog.setMessage("Silakan Tunggu...");
+                    progressDialog.setCancelable(false);
+                    progressDialog.show();
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+                    return true;
+                }
+                return false;
+            }
+        });
 
     }
 
     void setupRecyclerView(){
+        Query query = Utility.getCollectionReferenceForNotes().orderBy("timestamp", Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<Note> options = new FirestoreRecyclerOptions.Builder<Note>()
+                .setQuery(query, Note.class).build();
+        rv_note_item.setLayoutManager(new LinearLayoutManager(this));
+        noteAdapter = new NoteAdapter(options, this);
+        rv_note_item.setAdapter(noteAdapter);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        noteAdapter.stopListening();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        noteAdapter.notifyDataSetChanged();
     }
 }
